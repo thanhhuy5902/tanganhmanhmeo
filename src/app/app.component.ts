2d7f66f57a31601050fcf75a0b6b9032b91e94fe
thanhhuy5902 <tranthanhhuy42@gmail.com>
2024-08-05 23:47:36 +0700

    test theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {MatRadioButton} from "@angular/material/radio";
+import {MatButtonToggle} from "@angular/material/button-toggle";
+import {MatIcon} from "@angular/material/icon";
+import {
+  MatAnchor,
+  MatButton,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconButton,
+  MatMiniFabButton
+} from "@angular/material/button";
+import {MatDivider} from "@angular/material/divider";
+import {signInWithPopup,Auth, GoogleAuthProvider} from "@angular/fire/auth";
+import {MatCheckbox} from "@angular/material/checkbox";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatRadioButton, MatButtonToggle, MatIcon, MatButton, MatMiniFabButton, MatDivider, MatFabButton, MatIconButton, MatAnchor, MatFabAnchor, RouterLink, MatCheckbox],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'testTheme';
+  currentUser: any; // Tạo biến currentUser để lưu thông tin người dùng hiện tại
+  constructor(private auth: Auth) {} // Inject Auth vào constructor
+  async login(){ // Tạo hàm login
+    const credential = await signInWithPopup(this.auth, new GoogleAuthProvider());
+    // Sử dụng signInWithPopup để đăng nhập bằng Google
+    this.currentUser = credential.user; // Lưu thông tin người dùng vào biến currentUser
+    console.log(this.currentUser); // Log ra thông tin người dùng
+    const token = await credential.user.getIdToken(); // Lấy token của người dùng
+    console.log(token); // Log ra token
+  }
+
+
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,20 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({
+    "projectId":"social-network-29cc2",
+    "appId":"1:254585183877:web:364e6b675665d5413fd3ec",
+    "storageBucket":"social-network-29cc2.appspot.com",
+    "apiKey":"AIzaSyBpQV6zJ6jTgOJJ47S77IwzSgXEnGi6XR0",
+    "authDomain":"social-network-29cc2.firebaseapp.com",
+    "messagingSenderId":"254585183877"
+  })),
+    provideAuth(() => getAuth()),
+    provideStorage(() => getStorage()), provideAnimationsAsync()]
 };
